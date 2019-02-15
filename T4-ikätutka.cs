28151907be22fe9a9f5d53db23d36b08d0d91c2f
Arttu Pylkkö <rudezki@gmail.com>
2019-02-15 10:58:39 +0200

    Lisätty vastaus yli 56-vuotiaille

--- T4-ikätutka.cs
+++ T4-ikätutka.cs
@@ -19,25 +19,35 @@ namespace Ikätutka
 			{
 				Console.Write("Mitä poitsu?");
 				Console.ReadKey();
-			}	else if (sukup == "m" && age > 13 && age < 26)
+			}
+			else if (sukup == "m" && age > 13 && age < 26)
 			{
 				Console.Write("Mitä nuorimies");
 				Console.ReadKey();
-			}	else if (sukup == "m" && age > 25 && age < 57)
+			}
+			else if (sukup == "m" && age > 25 && age < 57)
 			{
 				Console.Write("Mies parhaassa iässä");
 				Console.ReadKey();
-			}	else if (sukup == "n" && age < 13)
+			}
+			else if (sukup == "n" && age < 13)
 			{
 				Console.Write("Mitä tyllerö?");
 				Console.ReadKey();
-			}	else if (sukup == "n" && age > 13 && age < 26)
+			}
+			else if (sukup == "n" && age > 13 && age < 26)
 			{
 				Console.Write("Mitä nuori naikkonen?");
 				Console.ReadKey();
-			}	else if (sukup == "n" && age > 25 && age < 57)
+			}
+			else if (sukup == "n" && age > 25 && age < 57)
 			{
 				Console.Write("Nainen kauneimmillaan");
+				Console.ReadKey();
+			}
+			else if (age > 56)
+			{
+				Console.Write("Vanheneminen on kaunista");
 				Console.ReadKey();
 			}
 		}
