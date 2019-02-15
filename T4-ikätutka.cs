3ed061e82477105d0f5514bd1dc6384f22ac9565
Arttu Pylkkö <rudezki@gmail.com>
2019-02-15 10:20:12 +0200

    First try age feedback

---
+++ T4-ikätutka.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ikätutka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mikä on ikäsi?");
+            string syote = Console.ReadLine();
+            int age = int.Parse(syote);
+            if (age < 13)
+            {
+                Console.Write("Mitä poitsu?");
+                Console.ReadKey();
+            } else if (age > 13 && age < 26) {
+                Console.Write("Mitä nuorimies");
+                Console.ReadKey();
+            } else if (age > 25 && age < 57)
+            {
+                Console.Write("Mies parhaassa iässä");
+                Console.ReadKey();
+            }
+        }
+    }
+}
