fe3f26ad3a846fbce42d05a111b8c6669a5f9158
Arttu Pylkkö <rudezki@gmail.com>
2019-02-15 11:56:04 +0200

    Initial commit

---
+++ T6-Forloops.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int i;
+			for (i = 1; i <= 10; i++)
+			{
+				Console.WriteLine("{0}", i);
+			}
+			Console.ReadKey();
+		}
+	}
+}
