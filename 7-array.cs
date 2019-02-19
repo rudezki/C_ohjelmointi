b3826380e6bd6332e4c082a50696021d2850331b
Arttu Pylkkö <rudezki@gmail.com>
2019-02-19 08:54:52 +0200

    Added question about how many fruits are shown

--- 7-array.cs
+++ 7-array.cs
@@ -11,8 +11,11 @@ namespace _7_array
 		static void Main(string[] args)
 		{
 			string[] fruit;
+			Console.WriteLine("How many fruits?");
+			string syote = Console.ReadLine();
+			int h = int.Parse(syote);
 			fruit = new string[3] { "Omena", "Appelsiini", "Banaani" };
-			for (int i = 0; i < fruit.Length; i++)
+			for (int i = 0; i < h; i++)
 			{
 				Console.WriteLine(fruit[i]);
 			}
