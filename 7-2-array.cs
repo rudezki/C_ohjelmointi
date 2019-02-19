9fb47202bf4df774079cc26b5e1a90181625c86a
Arttu Pylkkö <rudezki@gmail.com>
2019-02-19 09:58:57 +0200

    Initial commit

---
+++ 7-2-array.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_2_array
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.OutputEncoding = Encoding.UTF8;
+			string[] fruit;
+			double[] price;
+			fruit = new string[3] { "Omena ", "Päärynä ", "Tomaatti " };
+			price = new double[3] { 1.2, 1, 2 };
+
+			for (int i = 0; i < fruit.Length ; i++) {
+				Console.Write(fruit[i]);
+				Console.Write(price[i]);
+				Console.Write("€\r\n");
+			}
+			Console.ReadKey();
+		}
+	}
+}
