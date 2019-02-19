09f2346229f4bd5182a503350981e0bd138898b5
Arttu Pylkkö <rudezki@gmail.com>
2019-02-19 12:24:10 +0200

    Nyt voi kertoa ohjelmalle taulukoitavat asiat

--- 7-3-array.cs
+++ 7-3-array.cs
@@ -10,14 +10,23 @@ namespace _7_3_array
 	{
 		static void Main(string[] args)
 		{
+			string syote;
 			Console.OutputEncoding = Encoding.UTF8;
-			double[] tuoteHinta = new double[3] { 1.45, 3.40, 2 };
-			int[] tuoteLkm = new int[3] { 45, 30, 17 } ;
-			string[] tuoteNimi = new string[3] { "Omena", "Ananas", "Vesimeloni" };
+			double[] tuoteHinta = new double[3];
+			int[] tuoteLkm = new int[3];
+			string[] tuoteNimi = new string[3];
 			for (int i = 0; i < tuoteNimi.Length; i++)
 			{
-				Console.WriteLine();
-				Console.Write("{0} yhteishinta on {1}€",tuoteNimi[i],tuoteHinta[i]*tuoteLkm[i]);
+				Console.WriteLine("Anna tuotteen {0} nimi:", i+1);
+				syote = Console.ReadLine();
+				tuoteNimi[i]= syote;
+				Console.WriteLine("Anna tuotteen {0} hinta:", i+1);
+				syote = Console.ReadLine();
+				tuoteHinta[i] = double.Parse(syote);
+				Console.WriteLine("Anna tuotteen {0} lukumäärä:", i+1);
+				syote = Console.ReadLine();
+				tuoteLkm[i] = int.Parse(syote);
+				Console.Write("Tuotteen {0} yhteishinta on {1}€\r\n",tuoteNimi[i],tuoteHinta[i]*tuoteLkm[i]);
 			}
 			Console.ReadKey();
 		
