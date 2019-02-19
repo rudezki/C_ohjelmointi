4cb2a476053c222d93ae296a3075f2f0f002d170
Arttu Pylkkö <rudezki@gmail.com>
2019-02-19 08:47:33 +0200

    Initial commit

---
+++ 7-array.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_array
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] fruit;
+			fruit = new string[3] { "Omena", "Appelsiini", "Banaani" };
+			for (int i = 0; i < fruit.Length; i++)
+			{
+				Console.WriteLine(fruit[i]);
+			}
+				Console.ReadKey();
+		}
+	}
+}
