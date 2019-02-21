32a2fe182b5db750091b9c6d7d87194b165b9847
Arttu Pylkkö <rudezki@gmail.com>
2019-02-21 10:05:28 +0200

    Initial commit

---
+++ 10.1-nimilista.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10._1_lista
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string syote;
+			List<string> lista = new List<string>();
+			Console.WriteLine("Anna nimiä (kirjoita 'quit' lopettaaksesi)");
+			for (; ;)
+			{
+				syote = Console.ReadLine();
+				if (syote == "quit")
+				{
+					Console.WriteLine("\n");
+					lista.ForEach(el => Console.WriteLine(el));
+					Console.ReadKey();
+					Environment.Exit(-1);
+				}
+				lista.Add(syote);
+			}
+
+		}
+	}
+}
