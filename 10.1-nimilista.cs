ec191d8243bbddde574d218951324fc46b71f555
Arttu Pylkkö <rudezki@gmail.com>
2019-02-21 12:47:12 +0200

    Tehtävä 11: Muunnettu ohjelma kaatumattomaksi

--- 10.1-nimilista.cs
+++ 10.1-nimilista.cs
@@ -11,17 +11,26 @@ namespace _10._1_lista
 		static void Main(string[] args)
 		{
 			string syote;
-			int i;
+			int i, number;
+			bool result;
 			List<string> lista = new List<string>();
 			Console.WriteLine("Anna nimiä (kirjoita 'quit' lopettaaksesi)");
+
 			for (; ;)
 			{
 				syote = Console.ReadLine();
 				if (syote == "quit")
 				{
 					Console.WriteLine("\n Tulostetaanko nimet (a)lku- vai (l)oppupäästä?");
+
+					Question:
 					syote = Console.ReadLine();
 					lista.Sort();
+					result = Int32.TryParse(syote, out number);
+					if (result)
+					{
+						Console.WriteLine("Converted {0} to {1}.", syote, number);
+					}
 					if (syote == "a")
 					{
 						for (i = 0; i < lista.Count; i++)
@@ -37,7 +46,12 @@ namespace _10._1_lista
 							Console.WriteLine(lista[i]);
 						}
 					}
-						Console.ReadKey();
+					else
+					{
+						Console.WriteLine("Attempted conversion of '{0}' failed", syote == null ? "<null>" : syote);
+						goto Question;
+					}
+					Console.ReadKey();
 					Environment.Exit(-1);
 				}
 				lista.Add(syote);
