d0f3891951ae65158d9eadfd5d287a54e34ea77c
Arttu Pylkkö <rudezki@gmail.com>
2019-02-22 10:08:24 +0200

    Initial commit

---
+++ 12-jako-ohjelma.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_trycatch
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string syote;
+			int luku, jako;
+	
+
+			try
+			{
+				Console.WriteLine("Syötä jaettava luku");
+				syote = Console.ReadLine();
+				luku = int.Parse(syote);
+				Console.WriteLine("Syötä jakaja");
+				syote = Console.ReadLine();
+				jako = int.Parse(syote);
+				int tulos = luku / jako;
+				Console.WriteLine("Jakosuhde on {0}", tulos);
+				Console.ReadKey();
+			}
+			catch
+			{
+				Console.WriteLine("On tapahtunut virhe!");
+			}
+			finally
+			{
+				Console.WriteLine("Kiitos kun käytit hienoa ohjelmaani! Näkemiin!");
+				Console.ReadKey();
+			}
+		}
+	}
+}
