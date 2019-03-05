902acb7e0775087ce59f2dc623abc41020f2549f
Arttu Pylkkö <rudezki@gmail.com>
2019-03-05 09:08:05 +0200

    Initial commit

---
+++ 12-2-trycatch.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_trycatch
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string syote;
+			int luku, jako;
+
+
+			try
+			{
+				Console.WriteLine("Syötä jaettava luku");
+				syote = Console.ReadLine();
+				luku = int.Parse(syote);
+
+				Console.WriteLine("Syötä jakaja");
+				syote = Console.ReadLine();
+				jako = int.Parse(syote);
+
+				int tulos = luku / jako;
+				Console.WriteLine("Jakosuhde on {0}", tulos);
+				Console.ReadKey();
+			}
+			catch (DivideByZeroException e)
+			{
+				Console.WriteLine("Virhe! {0}", e.Message);
+			}
+			catch (FormatException e)
+			{
+				Console.WriteLine("Virhe! {0}", e.Message);
+			}
+			finally
+			{
+				Console.WriteLine("Kiitos kun käytit hienoa ohjelmaani! Näkemiin!");
+				Console.ReadKey();
+			}
+		}
+	}
+}
