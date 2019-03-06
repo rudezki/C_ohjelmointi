8b48435e5311bb818d5e077da96b34cfffd60a31
Arttu Pylkkö <rudezki@gmail.com>
2019-03-06 10:07:17 +0200

    Initial commit

---
+++ 13,5-1-nameage.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_5_1_nimijaikä
+{
+	class Program
+	{
+
+		enum Valikko {Name = 1, Age, ShowName, ShowAge, Exit};
+		static void Main(string[] args)
+		{
+			int valinta;
+			Valikko nimi = Valikko.Name;
+			Valikko ikä = Valikko.Age;
+			Valikko showname = Valikko.ShowName;
+			Valikko showage = Valikko.ShowAge;
+			Valikko exit = Valikko.Exit;
+
+			Alku:
+			try
+			{
+				Console.WriteLine("1.Anna nimesi");
+				Console.WriteLine("2.Anna ikäsi");
+				Console.WriteLine("3.Näytä nimesi");
+				Console.WriteLine("4.Näytä ikäsi");
+				Console.WriteLine("5.Poistu ohjelmasta");
+				valinta = int.Parse(Console.ReadLine());
+				switch (valinta)
+				{
+					case Valikko.Name:
+						Console.WriteLine("Syötä nimi:");
+							nimi = Console.ReadLine();
+						break;
+
+					case Valikko.Age:
+						Console.WriteLine("Syötä ikä:");
+						ikä = int.Parse(Console.ReadLine());
+						break;
+
+					case Valikko.ShowName:
+						Console.WriteLine("Nimesi on: {0}", showname);
+						break;
+
+					case Valikko.ShowAge:
+						Console.WriteLine("Ikäsi on: {0}", showage);
+						break;
+
+					case Valikko.Exit:
+						Console.WriteLine("Näkemiin!");
+						Environment.Exit(-1);
+						break;
+				}
+				goto Alku;
+			}
+			catch (FormatException)
+			{
+				Console.WriteLine("Virhe! Et syöttänyt numeroa!");
+			}
+			finally
+			{
+				Console.ReadKey();
+			}
+
+		}
+	}
+}
