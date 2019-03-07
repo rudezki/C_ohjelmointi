54f40aaa16d865f0142e97ce018c28f71ce9dbd9
Arttu Pylkkö <rudezki@gmail.com>
2019-03-07 09:10:08 +0200

    Muokattiin toimivaksi

--- 13,5-1-nameage.cs
+++ 13,5-1-nameage.cs
@@ -12,13 +12,9 @@ namespace _13_5_1_nimijaikä
 		enum Valikko {Name = 1, Age, ShowName, ShowAge, Exit};
 		static void Main(string[] args)
 		{
-			int valinta;
-			Valikko nimi = Valikko.Name;
-			Valikko ikä = Valikko.Age;
-			Valikko showname = Valikko.ShowName;
-			Valikko showage = Valikko.ShowAge;
-			Valikko exit = Valikko.Exit;
-
+			string nimi = "";
+			int ikä = 0;
+			Console.WriteLine("Tervetuloa ohjelmaan. Siirry valikossa valitsemalla numero");
 			Alku:
 			try
 			{
@@ -26,31 +22,38 @@ namespace _13_5_1_nimijaikä
 				Console.WriteLine("2.Anna ikäsi");
 				Console.WriteLine("3.Näytä nimesi");
 				Console.WriteLine("4.Näytä ikäsi");
-				Console.WriteLine("5.Poistu ohjelmasta");
-				valinta = int.Parse(Console.ReadLine());
+				Console.WriteLine("5.Poistu ohjelmasta\n");
+				Console.Write("     >");
+				Valikko valinta = (Valikko)int.Parse(Console.ReadLine());
 				switch (valinta)
 				{
 					case Valikko.Name:
 						Console.WriteLine("Syötä nimi:");
 							nimi = Console.ReadLine();
+						Console.WriteLine("Nimesi on tallennettu.");
 						break;
 
 					case Valikko.Age:
 						Console.WriteLine("Syötä ikä:");
 						ikä = int.Parse(Console.ReadLine());
+						Console.WriteLine("Ikäsi on tallennettu.");
 						break;
 
 					case Valikko.ShowName:
-						Console.WriteLine("Nimesi on: {0}", showname);
+						Console.WriteLine("Nimesi on: {0}", nimi);
 						break;
 
 					case Valikko.ShowAge:
-						Console.WriteLine("Ikäsi on: {0}", showage);
+						Console.WriteLine("Ikäsi on: {0}", ikä);
 						break;
 
 					case Valikko.Exit:
 						Console.WriteLine("Näkemiin!");
+						Console.ReadKey();
 						Environment.Exit(-1);
+						break;
+					default:
+						Console.WriteLine("Et valinnut mitään!");
 						break;
 				}
 				goto Alku;
@@ -58,6 +61,7 @@ namespace _13_5_1_nimijaikä
 			catch (FormatException)
 			{
 				Console.WriteLine("Virhe! Et syöttänyt numeroa!");
+				goto Alku;
 			}
 			finally
 			{
