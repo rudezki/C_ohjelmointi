510f3d9ffa64d770faae43ad521cf3a4ea9b4ca2
Arttu Pylkkö <rudezki@gmail.com>
2019-03-07 10:03:15 +0200

    Initial commit

---
+++ 14-tekstinkäsittely.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_tekstinkäsittely
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string nimi;
+			int i;
+			char letter;
+
+			try
+			{
+				Console.WriteLine("Anna nimi!");
+				nimi = Console.ReadLine();
+				char[] array = nimi.ToCharArray();
+				Console.WriteLine("");
+
+				for (i = 0; i < nimi.Length; i++)
+				{
+					letter = array[i];
+					Console.Write(letter);
+				}
+				Console.Write("\n");
+				for (i = 0; i < nimi.Length; i++)
+				{
+					letter = array[i];
+					Console.Write(letter);
+					Console.Write(" ");
+				}
+				Console.Write("\n");
+				for (i = nimi.Length -1; i >= 0; i--)
+				{
+					letter = array[i];
+					Console.Write(letter);
+					Console.Write(" ");
+				}
+
+
+			}
+			catch (FormatException e)
+			{
+				Console.WriteLine("Virhe! ", e.Message);
+			}
+			finally
+			{
+				Console.ReadKey();
+			}
+		}
+	}
+}
