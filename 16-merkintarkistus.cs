eef19a58429a341709635fc42c4eaff3b909d263
Arttu Pylkkö <rudezki@gmail.com>
2019-03-12 09:07:36 +0200

    Initial commit

---
+++ 16-merkintarkistus.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_merkintarkistus
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Char merkki;
+			ConsoleKeyInfo syote;
+
+			Console.WriteLine("Syötä merkki!\n");
+
+			syote = Console.ReadKey();
+			merkki = syote.KeyChar;
+
+			if (Console.ReadKey().Key == ConsoleKey.Spacebar)
+			{
+				Console.WriteLine("Syötit tyhjän merkin");
+			}
+			else if (Char.IsDigit(merkki))
+			{
+				Console.WriteLine("Syötit numeron: {0}", merkki);
+			}
+			else if (Char.IsLetter(merkki))
+			{
+				Console.WriteLine("Syötit kirjaimen: {0}", merkki);
+			}
+			else if (Char.IsPunctuation(merkki));
+			{
+				Console.WriteLine("Syötit merkin: {0}", merkki);
+			}
+			Console.ReadKey();
+			}
+		}
+	}
+
