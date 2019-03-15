860ea9249ac6425d86d7fe01bd94224f5717d1ee
Arttu Pylkkö <rudezki@gmail.com>
2019-03-15 08:23:25 +0200

    Initial commit

---
+++ 20-alkuluvut.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_alkuluvut
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int luku;
+			Console.WriteLine("Anna luku!");
+			luku = int.Parse(Console.ReadLine());
+
+			if (luku % 2 == 0)
+			{
+				Console.WriteLine("Numero ei ole alkuluku!");
+			} else if (luku % 2 > 0)
+			{
+				Console.WriteLine("Numero on alkuluku!");
+			}
+			Console.ReadKey();
+			
+			
+		}
+	}
+}
