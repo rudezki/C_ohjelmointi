a7c8fea1f41cd51f45c7d8a97bbcb0e283b506f2
Arttu Pylkkö <rudezki@gmail.com>
2019-03-19 12:09:43 +0200

    Initial commit

---
+++ 21-laivanupotus.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21_laivanupotus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z, w, x1, y1, z1 ,w1, number;
+            string syote;
+            bool result;
+
+            Random zKoordinaatti = new Random();
+            z = zKoordinaatti.Next(1, 5);
+            Random wKoordinaatti = new Random();
+            w = wKoordinaatti.Next(1, 5);
+            Console.WriteLine("Asetetaan laiva 5x5-koordinaatistoon!");
+
+
+         
+
+            Koordinaattix:
+                Console.WriteLine("Anna koordinaatti X");
+                syote = Console.ReadLine();
+                result = Int32.TryParse(syote, out number);
+                x = number;
+            if (x < 1 || x > 5)
+                {
+                    Console.WriteLine("Virheellinen koordinaatti!");
+                    goto Koordinaattix;
+                }
+                Console.WriteLine("Anna koordinaatti y");
+                Koordinaattiy:
+                syote = Console.ReadLine();
+                result = Int32.TryParse(syote, out number);
+                y = number;
+            if (x < 1 || x > 5)
+                {
+                    Console.WriteLine("Virheellinen koordinaatti!");
+                    goto Koordinaattiy;
+                }
+                Console.WriteLine("Sinä aloitat!");
+                for (; ; )
+                {
+                    Console.WriteLine("Ammuttava koordinaatti x!");
+                    syote = Console.ReadLine();
+                    result  = Int32.TryParse(syote, out number);
+                    x1 = number;
+                    Console.WriteLine("Ammuttava koordinaatti y!");
+                    syote = Console.ReadLine();
+                    result = Int32.TryParse(syote, out number);
+                    y1 = number;
+                    if (x1 == z && y1 == w)
+                    {
+                        Console.WriteLine("Osui ja upposi! Voitit pelin!");
+                        Console.ReadKey();
+                        Environment.Exit(-1);
+                    } else if (x1 < 1 || y1 < 1 || x1 > 5 || y1 > 5)
+                    {
+                        Console.WriteLine("Virheellinen koordinaatti");
+                    }
+
+                        Random zAmmus = new Random();
+                        z1 = zAmmus.Next(1, 5);
+                        Random wAmmus = new Random();
+                        w1 = wAmmus.Next(1, 5);
+                        Console.WriteLine("Tietokone ampui koordinaatteihin {0} ja {1}", z1, w1);
+
+                    if (z1 == x && w1 == y)
+                    {
+                        Console.WriteLine("Aluksesi upposi! Hävisit pelin! :(");
+                        Console.ReadKey();
+                        Environment.Exit(-1);
+                    }
+
+                    Console.WriteLine("Kumpikaan ei voittanut! Uusi kierros!");
+
+
+                }
+                    
+
+            }
+    }
+}
