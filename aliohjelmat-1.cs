be98b9b46c969434f03b321ec64df78dc9c608c3
Arttu Pylkkö <rudezki@gmail.com>
2019-03-26 09:29:43 +0200

    Initial commit

---
+++ aliohjelmat-1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aliohjelmat_1
+{
+    class Program
+    {
+        private static void Tervehdys()
+        {
+            Console.WriteLine("Tervetuloa ohjelmaan!");
+        }
+
+        static void Main(string[] args)
+        {
+            Tervehdys();
+            Console.ReadKey();
+        }
+    }
+}
