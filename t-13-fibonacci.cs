d5a224eba97c0398d15fe8a67e5f4807e36bc13a
Arttu Pylkkö <rudezki@gmail.com>
2019-04-02 11:36:47 +0300

    Initial commit

---
+++ t-13-fibonacci.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T13_Fibonacci
+{
+    class Program
+    {
+        private static long Fibonacci(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            if (n == 1)
+            {
+                return 1;
+            }
+            if (n >= 2)
+            {
+                return Fibonacci(n - 2) + Fibonacci(n - 1);
+            }
+            else
+            {
+                return 0;
+            }
+            
+        }
+        static void Main(string[] args)
+        {
+            int i, x;
+            string syote;
+            bool result;
+
+            Console.WriteLine("Anna luku");
+            syote = Console.ReadLine();
+            result = Int32.TryParse(syote, out x);
+            for (i = 0; i <= x; i++)
+            {
+                Console.WriteLine(Fibonacci(i));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
