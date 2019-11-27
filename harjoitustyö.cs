b95b4a59fcd545979e6063104e908a22e155fd9d
Arttu Pylkkö <rudezki@gmail.com>
2019-11-27 12:48:22 +0200

    Uudelleentehty if-lausekkeet

--- harjoitustyö.cs
+++ harjoitustyö.cs
@@ -1,26 +1,21 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
-//using System.Text.RegularExpressions;
 
 namespace Harjoitustyö
 {
     class Program
     {
-        //Regexiä voidaan hyödyntää, että voidaan saada ohjelman olemaan huomioimatta välilyöntejä.
-        //private static readonly Regex normalizeSpace =
-        //new Regex(@"\s+", RegexOptions.Compiled);
+
         static void Main(string[] args)
         {
             try
             {
-                char previousKey = 'g';
+                char previousKey = ' ';
                 
-                bool lauseenLoppu = true, isoKirjain = true;
+                bool lauseenLoppu = true, isoKirjain = false;
                 List<string> outputText = new List<string>();
                 string inputText = File.ReadAllText("inputfile.txt");
-                //Tässä käytettäisiin RegExiä, jos haluttaisiin, ettei välilyöntiä oteta huomioon ollenkaan.
-                //string withoutSpaces = normalizeSpace.Replace(inputText, " ");
                 
 
                 foreach (char key in inputText)
@@ -28,39 +23,31 @@ namespace Harjoitustyö
                     //Kaksi peräkkäistä if-lauseketta, että tiedetään, miten seuraavan kirjaimen tulee käyttäytyä, nykyisen perusteella.
                     if (key == '.' || key == '?' || key == ':' || key == '!' || key == '\n')
                     {
-                        isoKirjain = false;
                         lauseenLoppu = true;
-                        previousKey = key;
                     }
-                    else if (lauseenLoppu && (!(key == ' ')))
+                    else if (previousKey == ' ' && char.IsUpper(key))
                     {
                         isoKirjain = true;
-                        previousKey = key;
-                    } else
-                    {
-                        isoKirjain = false;
-                        previousKey = key;
                     }
-                    if (isoKirjain)
+                    
+                    previousKey = key;
+
+                    if (lauseenLoppu)
                     {
                         outputText.Add(char.ToUpper(previousKey).ToString());
                         lauseenLoppu = false;
                     }
-                    else if (!isoKirjain && char.IsLower(key) && char.IsUpper(previousKey))
+                    else if (isoKirjain && char.IsUpper(previousKey) && char.IsLower(key))
                     {
-                        outputText.Add(char.ToLower(previousKey).ToString());
-                    }
-                    else if (!isoKirjain && char.IsUpper(key) && char.IsUpper(previousKey))
-                    {
-                        outputText.Add(char.ToLower(previousKey).ToString());
-                    }
+                        outputText.Add(char.ToUpper(previousKey).ToString());
+                        isoKirjain = false;
+                    } 
                     else
                     {
-                        outputText.Add(previousKey.ToString());
+                        outputText.Add(char.ToLower(previousKey).ToString());
                     }
-
                 }
-
+                //Kirjoitetaan listan sisältö tiedostoon.
                 using (StreamWriter outputFile = File.AppendText("outputfile.txt"))
                 {
                     foreach (string line in outputText)
@@ -68,18 +55,19 @@ namespace Harjoitustyö
                         outputFile.Write(line.ToString());
                     }
                 }
+                /*
                 string[] allLines = File.ReadAllLines("outputfile.txt");
                 foreach (string line in allLines)
                 {
                     Console.WriteLine(line);
                 }
+                Jos halutaan tietää mitä ollaan kirjoitettu, niin käytetään tätä koodinpätkää */
 
             }
             catch (IOException e)
             {
                 Console.WriteLine(e.Message);
             }
-            Console.ReadKey();
         }
     }
 }
