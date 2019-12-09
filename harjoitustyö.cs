635a4144e6de78c3258ede97e631e749725550a5
Arttu Pylkkö <rudezki@gmail.com>
2019-12-09 11:57:16 +0200

    säälittävää rassausta

--- harjoitustyö.cs
+++ harjoitustyö.cs
@@ -2,7 +2,7 @@
 using System.IO;
 using System.Collections.Generic;
 
-namespace Harjoitustyö
+namespace harjoitustyö2
 {
     class Program
     {
@@ -11,57 +11,84 @@ namespace Harjoitustyö
         {
             try
             {
-                char previousKey = ' ';
-                
-                bool lauseenLoppu = true, isoKirjain = false;
+                char previousKey = '.', previousPrevious = '.';
+
+                bool isoKirjain = true, lauseenLoppu = true, onEdellinen = false, onEdellinenEdellinen = false;
                 List<string> outputText = new List<string>();
                 string inputText = File.ReadAllText("inputfile.txt");
-                
+
 
                 foreach (char key in inputText)
                 {
                     //Kaksi peräkkäistä if-lauseketta, että tiedetään, miten seuraavan kirjaimen tulee käyttäytyä, nykyisen perusteella.
-                    if (key == '.' || key == '?' || key == ':' || key == '!' || key == '\n')
+                    //Jos kirjain on jokin näistä välimerkeistä, on virke loppunut ja seuraavan kirjaimen pitäisi olla iso.
+                    if (!onEdellinen)
+                    {
+                        previousKey = key;
+                        previousPrevious = previousKey;
+                        onEdellinen = true;
+                    } else if (!onEdellinenEdellinen && onEdellinen)
+                    {
+                        previousKey = key;
+                        previousPrevious = previousKey;
+                        onEdellinenEdellinen = true;
+                    } else
+                    {
+
+                    }
+                    
+                    if (previousPrevious == '.' || previousPrevious == '?' || previousPrevious == ':' || previousPrevious == '!' || previousPrevious == '\n')
                     {
                         lauseenLoppu = true;
                     }
-                    else if (previousKey == ' ' && char.IsUpper(key))
+                    if (onEdellinen && !char.IsPunctuation(previousKey) && !(previousKey == ' ') && char.IsUpper(previousKey) && char.IsLower(key))
                     {
                         isoKirjain = true;
-                    }
-                    
-                    previousKey = key;
-
-                    if (lauseenLoppu)
+                    } else if (onEdellinen && !(previousPrevious == ' ') && char.IsUpper(previousKey) && char.IsLower(key))
                     {
-                        outputText.Add(char.ToUpper(previousKey).ToString());
-                        lauseenLoppu = false;
-                    }
-                    else if (isoKirjain && char.IsUpper(previousKey) && char.IsLower(key))
-                    {
-                        outputText.Add(char.ToUpper(previousKey).ToString());
                         isoKirjain = false;
-                    } 
-                    else
+                    }
+                    if (isoKirjain && onEdellinen)
+                    {
+                        outputText.Add(char.ToUpper(previousKey).ToString());
+                        previousKey = key;
+                        previousPrevious = previousKey;
+                        isoKirjain = false;
+                    }
+                    else if (onEdellinen && lauseenLoppu)
+                    {
+                        outputText.Add(char.ToUpper(previousKey).ToString());
+                        previousKey = key;
+                        previousPrevious = previousKey;
+                        lauseenLoppu = false;
+                    } else if (onEdellinen && !isoKirjain)
                     {
                         outputText.Add(char.ToLower(previousKey).ToString());
-                    }
+                        previousKey = key;
+                        previousPrevious = previousKey;
+                        isoKirjain = false;
+                    } 
+
+
+
                 }
                 //Kirjoitetaan listan sisältö tiedostoon.
                 using (StreamWriter outputFile = File.AppendText("outputfile.txt"))
                 {
+                    outputFile.Write("\n");
                     foreach (string line in outputText)
                     {
                         outputFile.Write(line.ToString());
                     }
                 }
-                /*
+                
                 string[] allLines = File.ReadAllLines("outputfile.txt");
                 foreach (string line in allLines)
                 {
                     Console.WriteLine(line);
                 }
-                Jos halutaan tietää mitä ollaan kirjoitettu, niin käytetään tätä koodinpätkää */
+                Console.ReadKey();
+                //Jos halutaan tietää mitä ollaan kirjoitettu, niin käytetään tätä koodinpätkää 
 
             }
             catch (IOException e)
